f56afcebe415c772c2e15844cd6f1a6af7283432
Sean Bode <seanpaulbode@gmail.com>
2016-05-24 23:27:04 -0400

    Assignment 26 rTesting

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -15,4 +15,10 @@ describe "GET index" do
      end
    end
 
+describe "GET faq" do
+    it "renders the FAQ template" do
+      get :faq
+      expect(response).to render_template("faq")
+    end
+  end
 end
