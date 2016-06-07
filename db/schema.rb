936b002ea8051c1ab5c0d60ec963f5e0df3305b0
Sean Bode <seanpaulbode@gmail.com>
2016-06-07 14:33:41 -0400

    Removed Sponsored posts and scanned for errors.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606003145) do
+ActiveRecord::Schema.define(version: 20160604234237) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -31,15 +31,6 @@ ActiveRecord::Schema.define(version: 20160606003145) do
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
-
-  create_table "sponsored_posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "price"
-    t.integer  "topic_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
