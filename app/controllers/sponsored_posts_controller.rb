7517698cba8b2b99f3ee8e23ec4e2193f12970e9
Sean Bode <seanpaulbode@gmail.com>
2016-06-08 16:08:21 -0400

    Assignment 34 Sponsored Post: Fixed :price error

--- app/controllers/sponsored_posts_controller.rb
+++ app/controllers/sponsored_posts_controller.rb
@@ -13,14 +13,14 @@ class SponsoredPostsController < ApplicationController
   end
 
   def create
-  	@sponsored_post = Post.new
+  	@sponsored_post = SponsoredPost.new
   	@sponsored_post.title = params[:sponsored_post][:title]
   	@sponsored_post.body = params[:sponsored_post][:body]
     @sponsored_post.price = params[:sponsored_post][:price]
     @topic = Topic.find(params[:topic_id])
     @sponsored_post.topic = @topic
   	if @sponsored_post.save
-  		flash[:notice] = "Post was saved successfully."
+  		flash[:notice] = "Sponsored post was saved successfully."
   		redirect_to [@topic, @sponsored_post]
   	else
   		flash.now[:alert] = "There was an error saving your sponsored post. Please try again."
