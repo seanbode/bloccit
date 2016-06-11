f16119eb2113739c82739cadc4b12a71890bc2ad
Sean Bode <seanpaulbode@gmail.com>
2016-06-11 00:44:13 -0400

    Assignment 38 Gravatar

--- db/schema.rb
+++ db/schema.rb
@@ -32,15 +32,6 @@ ActiveRecord::Schema.define(version: 20160609012023) do
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
 
-  create_table "sponsored_posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "price"
-    t.integer  "topic_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "topics", force: :cascade do |t|
     t.string   "name"
     t.boolean  "public",      default: true
