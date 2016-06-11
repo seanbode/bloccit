d606012910427f87a295fabe47435fedbf7879ec
Sean Bode <seanpaulbode@gmail.com>
2016-06-11 02:27:03 -0400

    Checkpoint 40 Authorization

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,17 +4,17 @@ RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
 
   it { is_expected.to have_many(:posts) }
-# tests for name
+  # tests for name
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
 
-# tests for email
+  # tests for email
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email) }
   it { is_expected.to validate_length_of(:email).is_at_least(3) }
   it { is_expected.to allow_value("user@bloccit.com").for(:email) }
 
-# tests for password
+  # tests for password
   it { is_expected.to validate_presence_of(:password) }
   it { is_expected.to have_secure_password }
   it { is_expected.to validate_length_of(:password).is_at_least(6) }
@@ -35,6 +35,49 @@ RSpec.describe User, type: :model do
 
     it "should be an invalid user due to blank email" do
       expect(user_with_invalid_email).to_not be_valid
+    end
+  end
+
+  it "responds to role" do
+    expect(user).to respond_to(:role)
+  end
+
+  it "responds to admin?" do
+    expect(user).to respond_to(:admin?)
+  end
+
+  it "responds to member?" do
+    expect(user).to respond_to(:member?)
+  end
+end
+
+describe "roles" do
+  it "is member by default" do
+    expect(user.role).to eq("member")
+  end
+
+
+  context "member user" do
+    it "returns true for #member?" do
+      expect(user.member?).to be_truthy
+    end
+
+    it "returns false for #admin?" do
+      expect(user.admin?).to be_falsey
+    end
+  end
+
+  context "admin user" do
+    before do
+      user.admin!
+    end
+
+    it "returns false for #member?" do
+      expect(user.member?).to be_falsey
+    end
+
+    it "returns true for #admin?" do
+      expect(user.admin?).to be_truthy
     end
   end
 end
