0956df450d1fc734b380af3be32357aebd33f81f
Sean Bode <seanpaulbode@gmail.com>
2016-06-10 21:11:54 -0400

    Assignment 38 authenticate avatar

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
-  
-  resources :sessions, only: [:new, :create, :destroy]
+
+  resources :sessions, only: [:new, :create, :destroy, :avatar_url]
 
   get 'about' => 'welcome#about'
 
