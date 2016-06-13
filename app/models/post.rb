c1b4ec06ddc8a763cca2531045bc0e31fa6385bd
Sean Bode <seanpaulbode@gmail.com>
2016-06-13 17:50:28 -0400

    a45 vote spec factory error

--- app/models/post.rb
+++ app/models/post.rb
@@ -31,6 +31,6 @@ class Post < ActiveRecord::Base
   def update_rank
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
-    update_attribute(:rank, new_rank)
+    self.update(rank: new_rank)
   end
 end
