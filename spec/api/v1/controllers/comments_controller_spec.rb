fbbaf0bf92c8639982ec888c24a026900d661a31
Sean Bode <seanpaulbode@gmail.com>
2016-06-15 15:34:21 -0400

    Assignment 48 API

--- spec/api/v1/controllers/comments_controller_spec.rb
+++ spec/api/v1/controllers/comments_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Api::V1::CommentsController, type: :controller do
   let(:my_post) { create(:post) }
   let(:my_comment) { Comment.create!(body: RandomData.random_paragraph, post: my_post, user: my_user) }
 
-  context "unauthenticated user"
+  context "unauthenticated user" do
     it "GET index return http success" do
       get :index
       expect(response).to have_http_status(:success)
@@ -47,3 +47,4 @@ RSpec.describe Api::V1::CommentsController, type: :controller do
       expect(response_hash['body']).to eq my_comment.body
     end
   end
+end
--- spec/api/v1/controllers/posts_controller_spec.rb
+++ spec/api/v1/controllers/posts_controller_spec.rb
@@ -6,19 +6,24 @@ RSpec.describe Api::V1::PostsController, type: :controller do
   let(:my_post) { create(:post) }
   let(:my_comment) { Comment.create!(body: RandomData.random_paragraph, post: my_post, user: my_user) }
 
-  context "unauthenticated user"
+  context "unauthenticated user" do
+    #before do
+    #  my_user.admin!
+    #  controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
+    #end
+
     it "GET index return http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
 
     it "GET show returns http success" do
-      get :show, id: my_topic.id
+      get :show, id: my_post.id
       expect(response).to have_http_status(:success)
     end
 
     it "GET show returns child comments" do
-      get :show, id: my_topic.id
+      get :show, id: my_post.id
       response_hash = JSON.parse response.body
       expect(response_hash['comments']).to_not be_nil
     end
@@ -35,13 +40,14 @@ RSpec.describe Api::V1::PostsController, type: :controller do
     end
 
     it "GET show returns http success" do
-      get :show, id: my_topic.id
+      get :show, id: my_post.id
       expect(response).to have_http_status(:success)
     end
 
     it "GET show returns child comments" do
-      get :show, id: my_topic.id
+      get :show, id: my_post.id
       response_hash = JSON.parse response.body
       expect(response_hash['comments']).to_not be_nil
     end
   end
+end
